4825ae6d95bae65adb97ca887fde358f6895b4b6
Joseph An <an.joseph891@gmail.com>
2017-02-01 12:38:16 -0500

    Finish game

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+	public float upForce = 200f;
+
+	private bool isDead = false;
+	private Rigidbody2D rb;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isDead) {
+			if (Input.GetMouseButton(0)) {
+				rb.velocity = Vector2.zero;
+				rb.AddForce(new Vector2(0, upForce));
+				anim.SetTrigger("Flap");
+			}
+		}
+	}
+
+	void OnCollisionEnter2D() {
+		rb.velocity = Vector2.zero;
+		isDead = true;
+		anim.SetTrigger("Die");
+		GameControl.instance.BirdDied();
+	}
+}
---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Column : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.GetComponent<Bird>() != null) {
+			GameControl.instance.BirdScored();
+		}
+	}
+}
